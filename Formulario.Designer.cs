c2bee725a4028948f6cc93e8718403e6970f2bfd
acf0x <alcasfer96@gmail.com>
2024-07-23 20:47:51 +0200

    v1.0

---
+++ Formulario.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace GestorDeContraseñas
+{
+    partial class Formulario
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxContraseña = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBoxMinusculas = new System.Windows.Forms.CheckBox();
+            this.checkBoxMayusculas = new System.Windows.Forms.CheckBox();
+            this.checkBoxNumeros = new System.Windows.Forms.CheckBox();
+            this.checkBoxSimbolos = new System.Windows.Forms.CheckBox();
+            this.buttonGenerar = new System.Windows.Forms.Button();
+            this.buttonCopiar = new System.Windows.Forms.Button();
+            this.buttonAnadir = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ContraseñaColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TagColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonMostrar = new System.Windows.Forms.Button();
+            this.labelGithub = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gestor de contraseñas v1.0";
+            // 
+            // textBoxContraseña
+            // 
+            this.textBoxContraseña.Location = new System.Drawing.Point(16, 42);
+            this.textBoxContraseña.Name = "textBoxContraseña";
+            this.textBoxContraseña.Size = new System.Drawing.Size(370, 20);
+            this.textBoxContraseña.TabIndex = 1;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(16, 68);
+            this.trackBar1.Maximum = 24;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(370, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 12;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(392, 68);
+            this.numericUpDown1.Maximum = new decimal(new int[] {24,0,0,0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {1,0,0,0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {12,0,0,0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // checkBoxMinusculas
+            // 
+            this.checkBoxMinusculas.AutoSize = true;
+            this.checkBoxMinusculas.Location = new System.Drawing.Point(16, 119);
+            this.checkBoxMinusculas.Name = "checkBoxMinusculas";
+            this.checkBoxMinusculas.Size = new System.Drawing.Size(79, 17);
+            this.checkBoxMinusculas.TabIndex = 4;
+            this.checkBoxMinusculas.Text = "Minúsculas";
+            this.checkBoxMinusculas.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxMayusculas
+            // 
+            this.checkBoxMayusculas.AutoSize = true;
+            this.checkBoxMayusculas.Location = new System.Drawing.Point(99, 119);
+            this.checkBoxMayusculas.Name = "checkBoxMayusculas";
+            this.checkBoxMayusculas.Size = new System.Drawing.Size(82, 17);
+            this.checkBoxMayusculas.TabIndex = 5;
+            this.checkBoxMayusculas.Text = "Mayúsculas";
+            this.checkBoxMayusculas.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxNumeros
+            // 
+            this.checkBoxNumeros.AutoSize = true;
+            this.checkBoxNumeros.Location = new System.Drawing.Point(189, 119);
+            this.checkBoxNumeros.Name = "checkBoxNumeros";
+            this.checkBoxNumeros.Size = new System.Drawing.Size(68, 17);
+            this.checkBoxNumeros.TabIndex = 6;
+            this.checkBoxNumeros.Text = "Números";
+            this.checkBoxNumeros.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxSimbolos
+            // 
+            this.checkBoxSimbolos.AutoSize = true;
+            this.checkBoxSimbolos.Location = new System.Drawing.Point(263, 119);
+            this.checkBoxSimbolos.Name = "checkBoxSimbolos";
+            this.checkBoxSimbolos.Size = new System.Drawing.Size(70, 17);
+            this.checkBoxSimbolos.TabIndex = 7;
+            this.checkBoxSimbolos.Text = "Símbolos";
+            this.checkBoxSimbolos.UseVisualStyleBackColor = true;
+            // 
+            // buttonGenerar
+            // 
+            this.buttonGenerar.Location = new System.Drawing.Point(16, 142);
+            this.buttonGenerar.Name = "buttonGenerar";
+            this.buttonGenerar.Size = new System.Drawing.Size(75, 23);
+            this.buttonGenerar.TabIndex = 8;
+            this.buttonGenerar.Text = "Generar";
+            this.buttonGenerar.UseVisualStyleBackColor = true;
+            this.buttonGenerar.Click += new System.EventHandler(this.buttonGenerar_Click);
+            // 
+            // buttonCopiar
+            // 
+            this.buttonCopiar.Location = new System.Drawing.Point(97, 142);
+            this.buttonCopiar.Name = "buttonCopiar";
+            this.buttonCopiar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCopiar.TabIndex = 9;
+            this.buttonCopiar.Text = "Copiar";
+            this.buttonCopiar.UseVisualStyleBackColor = true;
+            this.buttonCopiar.Click += new System.EventHandler(this.buttonCopiar_Click);
+            // 
+            // buttonAnadir
+            // 
+            this.buttonAnadir.Location = new System.Drawing.Point(178, 142);
+            this.buttonAnadir.Name = "buttonAnadir";
+            this.buttonAnadir.Size = new System.Drawing.Size(75, 23);
+            this.buttonAnadir.TabIndex = 10;
+            this.buttonAnadir.Text = "Añadir";
+            this.buttonAnadir.UseVisualStyleBackColor = true;
+            this.buttonAnadir.Click += new System.EventHandler(this.buttonAnadir_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ContraseñaColumn,
+            this.TagColumn});
+            this.dataGridView1.Location = new System.Drawing.Point(16, 171);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(432, 150);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // ContraseñaColumn
+            // 
+            this.ContraseñaColumn.HeaderText = "Contraseña";
+            this.ContraseñaColumn.Name = "ContraseñaColumn";
+            this.ContraseñaColumn.ReadOnly = true;
+            // 
+            // TagColumn
+            // 
+            this.TagColumn.HeaderText = "Tag";
+            this.TagColumn.Name = "TagColumn";
+            // 
+            // buttonMostrar
+            // 
+            this.buttonMostrar.Location = new System.Drawing.Point(259, 142);
+            this.buttonMostrar.Name = "buttonMostrar";
+            this.buttonMostrar.Size = new System.Drawing.Size(75, 23);
+            this.buttonMostrar.TabIndex = 12;
+            this.buttonMostrar.Text = "Mostrar";
+            this.buttonMostrar.UseVisualStyleBackColor = true;
+            this.buttonMostrar.Click += new System.EventHandler(this.buttonMostrar_Click);
+            // 
+            // labelGithub
+            // 
+            this.labelGithub.AutoSize = true;
+            this.labelGithub.Location = new System.Drawing.Point(16, 324);
+            this.labelGithub.Name = "labelGithub";
+            this.labelGithub.Size = new System.Drawing.Size(90, 13);
+            this.labelGithub.TabIndex = 13;
+            this.labelGithub.Text = "github.com/acf0x";
+            // 
+            // Formulario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(461, 346);
+            this.Controls.Add(this.labelGithub);
+            this.Controls.Add(this.buttonMostrar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonAnadir);
+            this.Controls.Add(this.buttonCopiar);
+            this.Controls.Add(this.buttonGenerar);
+            this.Controls.Add(this.checkBoxSimbolos);
+            this.Controls.Add(this.checkBoxNumeros);
+            this.Controls.Add(this.checkBoxMayusculas);
+            this.Controls.Add(this.checkBoxMinusculas);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.textBoxContraseña);
+            this.Controls.Add(this.label1);
+            this.Name = "Formulario";
+            this.Text = "Gestor de contraseñas v1.0";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxContraseña;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBoxMinusculas;
+        private System.Windows.Forms.CheckBox checkBoxMayusculas;
+        private System.Windows.Forms.CheckBox checkBoxNumeros;
+        private System.Windows.Forms.CheckBox checkBoxSimbolos;
+        private System.Windows.Forms.Button buttonGenerar;
+        private System.Windows.Forms.Button buttonCopiar;
+        private System.Windows.Forms.Button buttonAnadir;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ContraseñaColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TagColumn;
+        private System.Windows.Forms.Button buttonMostrar;
+        private System.Windows.Forms.Label labelGithub;
+    }
+}
---
+++ Formulario.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace GestorDeContraseñas
+{
+    public partial class Formulario : Form
+    {
+        private static readonly Random random = new Random();
+        private const string filePath = "contraseñas.txt";
+
+        public Formulario()
+        {
+            InitializeComponent();
+            trackBar1.Maximum = 24;
+            numericUpDown1.Maximum = 24;
+            trackBar1.Minimum = 1;
+            numericUpDown1.Minimum = 1;
+            numericUpDown1.Value = 12; // Valor por defecto
+            CargarContraseñas();
+        }
+
+        private void GenerarContraseña()
+        {
+            int length = (int)numericUpDown1.Value;
+            string chars = "";
+            if (checkBoxMinusculas.Checked) chars += "abcdefghijklmnopqrstuvwxyz";
+            if (checkBoxMayusculas.Checked) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            if (checkBoxNumeros.Checked) chars += "0123456789";
+            if (checkBoxSimbolos.Checked) chars += "!@#$%&.";
+
+            StringBuilder contraseña = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                contraseña.Append(chars[random.Next(chars.Length)]);
+            }
+
+            textBoxContraseña.Text = contraseña.ToString();
+        }
+
+        private void buttonGenerar_Click(object sender, EventArgs e)
+        {
+            GenerarContraseña();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = trackBar1.Value;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            trackBar1.Value = (int)numericUpDown1.Value;
+        }
+
+        private void buttonCopiar_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBoxContraseña.Text);
+            MessageBox.Show("Contraseña copiada al portapapeles.");
+        }
+
+        private void buttonAnadir_Click(object sender, EventArgs e)
+        {
+            string contraseña = textBoxContraseña.Text;
+            string tag = Prompt.ShowDialog("Ingrese un tag para la contraseña:", "Añadir Tag");
+
+            dataGridView1.Rows.Add("******", tag);
+            GuardarContraseña(contraseña, tag);
+        }
+
+        private void buttonMostrar_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[0].Value.ToString() == "******")
+                {
+                    row.Cells[0].Value = ObtenerContraseña(row.Index); // Método que obtiene la contraseña real
+                }
+                else
+                {
+                    row.Cells[0].Value = "******";
+                }
+            }
+        }
+
+        private string ObtenerContraseña(int rowIndex)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            string[] parts = lines[rowIndex].Split(new[] { "||" }, StringSplitOptions.None);
+            return Desencriptar(parts[0]);
+        }
+
+        private void GuardarContraseña(string contraseña, string tag)
+        {
+            using (StreamWriter sw = File.AppendText(filePath))
+            {
+                sw.WriteLine(Encriptar(contraseña) + "||" + tag);
+            }
+        }
+
+        private void CargarContraseñas()
+        {
+            if (File.Exists(filePath))
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(new[] { "||" }, StringSplitOptions.None);
+                    dataGridView1.Rows.Add("******", parts[1]);
+                }
+            }
+        }
+
+        private string Encriptar(string plainText)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(plainText);
+            using (Aes aes = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes("claveDeEncriptacion", new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                aes.Key = pdb.GetBytes(32);
+                aes.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(data, 0, data.Length);
+                        cs.Close();
+                    }
+                    plainText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return plainText;
+        }
+
+        private string Desencriptar(string cipherText)
+        {
+            cipherText = cipherText.Replace(" ", "+");
+            byte[] data = Convert.FromBase64String(cipherText);
+            using (Aes aes = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes("claveDeEncriptacion", new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                aes.Key = pdb.GetBytes(32);
+                aes.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(data, 0, data.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+    }
+}
